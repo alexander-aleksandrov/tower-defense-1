088c60a1630372db0f5177c51e215ee8a5753766
Aleksandr Aleksandrov <aleksandrov.aleksander.a@gmail.com>
2022-11-13 20:14:46 +0200

    Add game board initialization with path finding function

--- Assets/Scripts/GameBoard.cs
+++ Assets/Scripts/GameBoard.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GameBoard : MonoBehaviour
@@ -11,6 +12,8 @@ public class GameBoard : MonoBehaviour
 
 	private Vector2Int _size;
 
+	private Queue<Tile> _searchFrontier = new Queue<Tile>();
+
 	public void Initialyze(Vector2Int size)
 	{
 		_size = size;
@@ -21,12 +24,68 @@ public class GameBoard : MonoBehaviour
 		_tiles = new Tile[size.x * size.y];
 		for (int i = 0, y = 0; y < size.y; y++)
 		{
-			for (int x = 0; x < size.x; x++)
+			for (int x = 0; x < size.x; x++, i++)
 			{
 				Tile tile = _tiles[i] = Instantiate(_tilePrefab);
 				tile.transform.SetParent(transform, false);
 				tile.transform.localPosition = new Vector3(x - offset.x, 0f, y - offset.y);
+
+				if (x > 0)
+				{
+					Tile.MakeEastWestNeighbors(tile, _tiles[i - 1]);
+				}
+				if (y > 0)
+				{
+					Tile.MakeNorthSouthNeighbors(tile, _tiles[i - size.x]);
+				}
+
+				tile.IsAlternative = (x & 1) == 0;
+				if ((y & 1) == 0)
+				{
+					tile.IsAlternative = !tile.IsAlternative;
+				}
+
 			}
+		}
+		FindPath();
+	}
+
+	public void FindPath()
+	{
+		foreach (var tile in _tiles)
+		{
+			tile.ClearPath();
+		}
+		int destinationIndex = _tiles.Length / 2;
+		_tiles[destinationIndex].BecomeDestination();
+		_searchFrontier.Enqueue(_tiles[destinationIndex]);
+
+		while (_searchFrontier.Count > 0)
+		{
+			Tile tile = _searchFrontier.Dequeue();
+			if (tile != null)
+			{
+				if (tile.IsAlternative)
+				{
+					_searchFrontier.Enqueue(tile.GrowPathNorth());
+					_searchFrontier.Enqueue(tile.GrowPathSouth());
+					_searchFrontier.Enqueue(tile.GrowPathEast());
+					_searchFrontier.Enqueue(tile.GrowPathWest());
+				}
+				else
+				{
+					_searchFrontier.Enqueue(tile.GrowPathWest());
+					_searchFrontier.Enqueue(tile.GrowPathEast());
+					_searchFrontier.Enqueue(tile.GrowPathSouth());
+					_searchFrontier.Enqueue(tile.GrowPathNorth());
+				}
+			}
+
+		}
+
+		foreach (var tile in _tiles)
+		{
+			tile.ShowPath();
 		}
 	}
 }
--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -4,4 +4,66 @@ public class Tile : MonoBehaviour
 {
     [SerializeField]
     private Transform _arrow;
+    private Tile _east, _west, _north, _south, _nextOnPath;
+    private int _distance;
+
+
+    private Quaternion _northRotation = Quaternion.Euler(90f, 0f, 0f);
+    private Quaternion _eastRotation = Quaternion.Euler(90f, 90f, 0f);
+    private Quaternion _southRotation = Quaternion.Euler(90f, 180f, 0f);
+    private Quaternion _westRotation = Quaternion.Euler(90f, 270f, 0f);
+
+
+    public bool HasPath => _distance != int.MaxValue;
+    public bool IsAlternative { get; set; }
+
+    public static void MakeEastWestNeighbors(Tile east, Tile west)
+    {
+        east._west = west;
+        west._east = east;
+    }
+    public static void MakeNorthSouthNeighbors(Tile north, Tile south)
+    {
+        north._south = south;
+        south._north = north;
+    }
+    public void ClearPath()
+    {
+        _distance = int.MaxValue;
+        _nextOnPath = null;
+    }
+    public void BecomeDestination()
+    {
+        _distance = 0;
+        _nextOnPath = null;
+    }
+    private Tile GrowPathTo(Tile neighbor)
+    {
+        if (!HasPath || neighbor == null || neighbor.HasPath)
+        {
+            return null;
+        }
+        neighbor._distance = _distance + 1;
+        neighbor._nextOnPath = this;
+        return neighbor;
+    }
+    public Tile GrowPathEast() => GrowPathTo(_east);
+    public Tile GrowPathWest() => GrowPathTo(_west);
+    public Tile GrowPathNorth() => GrowPathTo(_north);
+    public Tile GrowPathSouth() => GrowPathTo(_south);
+
+    public void ShowPath()
+    {
+        if (_distance == 0)
+        {
+            _arrow.gameObject.SetActive(false);
+            return;
+        }
+        _arrow.gameObject.SetActive(true);
+        _arrow.localRotation =
+            _nextOnPath == _north ? _northRotation :
+            _nextOnPath == _east ? _eastRotation :
+            _nextOnPath == _south ? _southRotation :
+            _westRotation;
+    }
 }
